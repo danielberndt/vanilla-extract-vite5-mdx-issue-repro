7bdc5241b49f2afbb6791f743cad74e38fc80b4b
Daniel Berndt <daniel.berndt@gmail.com>
2025-01-02 17:00:32 +0100

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import vanillaExtractPlugin from '@vanilla-extract/vite-plugin';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    // @ts-ignore
+    plugins: [vanillaExtractPlugin()],
+  },
+  integrations: [mdx()],
+});
